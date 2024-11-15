9a3798af358e7fc2c35c7116fa39877c7f982f32
TylerTMizuyabu <tyler.mizuyabu@ssc-spc.gc.ca>
2024-11-15 10:39:19 -0500

    gr02.1 initial draft

--- src/lambda/gc02_check_access_config_groups/app.py
+++ src/lambda/gc02_check_access_config_groups/app.py
@@ -3,9 +3,11 @@
 """
 import json
 import logging
+import re
 
 import boto3
 import botocore
+import botocore.exceptions
 
 # Logging setup
 logger = logging.getLogger()
@@ -14,6 +16,7 @@ logger.setLevel(logging.INFO)
 # Set to True to get the lambda to assume the Role attached on the Config Service
 ASSUME_ROLE_MODE = True
 DEFAULT_RESOURCE_TYPE = "AWS::::Account"
+GROUP_RESOURCE_TYPE = "AWS::IAM::Group"
 
 
 # This gets the client after assuming the Config service role
@@ -122,74 +125,190 @@ def build_evaluation(
     )
     return eval_cc
 
+def extract_bucket_name_and_key(object_path):
+    match = re.match(r"s3:\/\/([^/]+)\/((?:[^/]*/)*.*)", object_path)
+    if match:
+        bucket_name = match.group(1)
+        key_name = match.group(2)
+    else:
+        logger.error("Unable to parse S3 object path %s", object_path)
+        raise ValueError(f"Unable to parse S3 object path {object_path}")
+    return bucket_name,key_name
 
-def assess_iam_password_policy():
-    """Obtains the IAM Password Policy in the account and assesses it against the parameters"""
-    compliance_status = "COMPLIANT"
-    compliance_annotation = ""
+def read_s3_object(s3_file_path):
+    bucket, key = extract_bucket_name_and_key(s3_file_path)
+    response = AWS_S3_CLIENT.get_object(Bucket=bucket, Key=key)
+    rule_naming_convention = response.get("Body").read().decode("utf-8")
+    return rule_naming_convention
+
+def check_s3_object_exists(object_path):
+    """Check if the S3 object exists
+    Keyword arguments:
+    object_path -- the S3 object path
+    """
+    # parse the S3 path
+    bucket_name, key_name = extract_bucket_name_and_key(object_path)
     try:
-        # get the current policy
-        response = AWS_IAM_CLIENT.get_account_password_policy()
-        if response:
-            current_password_policy = response.get("PasswordPolicy", {})
-            if current_password_policy:
-                # we have a policy, let's check
-                if int(PASSWORD_ASSESSMENT_POLICY.get("MinimumPasswordLength", -1)) > 0:
-                    if current_password_policy.get("MinimumPasswordLength", -1) < PASSWORD_ASSESSMENT_POLICY.get("MinimumPasswordLength"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "MinimumPasswordLength;"
-                if int(PASSWORD_ASSESSMENT_POLICY.get("MaxPasswordAge", -1)) > 0:
-                    if current_password_policy.get("MaxPasswordAge", -1) < PASSWORD_ASSESSMENT_POLICY.get("MaxPasswordAge"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "MaxPasswordAge;"
-                if (int(PASSWORD_ASSESSMENT_POLICY.get("PasswordReusePrevention", -1)) > 0):
-                    if current_password_policy.get("PasswordReusePrevention", -1) < PASSWORD_ASSESSMENT_POLICY.get("PasswordReusePrevention"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "PasswordReusePrevention;"
-                # The Policy items below are ONLY assessed IF they are required (True)
-                if PASSWORD_ASSESSMENT_POLICY.get("RequireSymbols", False):
-                    if current_password_policy.get("RequireSymbols", False) != PASSWORD_ASSESSMENT_POLICY.get("RequireSymbols"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "RequireSymbols;"
-                if PASSWORD_ASSESSMENT_POLICY.get("RequireNumbers", False):
-                    if current_password_policy.get("RequireNumbers", False) != PASSWORD_ASSESSMENT_POLICY.get("RequireNumbers"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "RequireNumbers;"
-                if PASSWORD_ASSESSMENT_POLICY.get("RequireUppercaseCharacters", False):
-                    if current_password_policy.get("RequireUppercaseCharacters", False) != PASSWORD_ASSESSMENT_POLICY.get("RequireUppercaseCharacters"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "RequireUppercaseCharacters;"
-                if PASSWORD_ASSESSMENT_POLICY.get("RequireLowercaseCharacters", False):
-                    if current_password_policy.get("RequireLowercaseCharacters", False) != PASSWORD_ASSESSMENT_POLICY.get("RequireLowercaseCharacters"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "RequireLowercaseCharacters;"
-                if PASSWORD_ASSESSMENT_POLICY.get("AllowUsersToChangePassword", False):
-                    if current_password_policy.get("AllowUsersToChangePassword", False) != PASSWORD_ASSESSMENT_POLICY.get("AllowUsersToChangePassword"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "AllowUsersToChangePassword;"
-                if PASSWORD_ASSESSMENT_POLICY.get("ExpirePasswords", False):
-                    if current_password_policy.get("ExpirePasswords", False) != PASSWORD_ASSESSMENT_POLICY.get("ExpirePasswords"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "ExpirePasswords;"
-                if PASSWORD_ASSESSMENT_POLICY.get("HardExpiry", False):
-                    if current_password_policy.get("HardExpiry", False) != PASSWORD_ASSESSMENT_POLICY.get("HardExpiry"):
-                        compliance_status = "NON_COMPLIANT"
-                        compliance_annotation += "HardExpiry;"
-            else:
-                compliance_status = "NON_COMPLIANT"
-                compliance_annotation = "Empty password policy read. Unable to assess"
-                logger.error(compliance_annotation)
+        AWS_S3_CLIENT.head_object(Bucket=bucket_name, Key=key_name)
+    except botocore.exceptions.ClientError as err:
+        if err.response["Error"]["Code"] == "404":
+            # The object does not exist.
+            logger.info("Object %s not found in bucket %s", key_name, bucket_name)
+            return False
+        elif err.response["Error"]["Code"] == "403":
+            # AccessDenied
+            logger.info("Access denied to bucket %s", bucket_name)
+            return False
         else:
-            compliance_status = "NON_COMPLIANT"
-            compliance_annotation = "Empty password policy read. Unable to assess"
-            logger.error(compliance_annotation)
-    except botocore.exceptions.ClientError as ex:
-        compliance_status = "NON_COMPLIANT"
-        compliance_annotation = "Unable to get_account_password_policy. Unable to assess"
-        logger.error(compliance_annotation)
-        logger.error(ex)
-    return {"status": compliance_status, "annotation": compliance_annotation}
+            # Something else has gone wrong.
+            logger.error("Error trying to find object %s in bucket %s", key_name, bucket_name)
+            raise ValueError(f"Error trying to find object {key_name} in bucket {bucket_name}") from err
+    else:
+        # The object does exist.
+        return True
 
+def fetch_groups():
+    groups = []
+    marker = None
+    try:
+        while True:
+            response = AWS_IAM_CLIENT.list_groups(Marker=marker) if marker else AWS_IAM_CLIENT.list_groups()
+            groups = groups + response.get("Groups", [])
+            marker = response.get("Marker")
+            if not marker:
+                break
+    except botocore.exceptions.ClientError as ex:
+        ex.response["Error"]["Message"] = "InternalError"
+        ex.response["Error"]["Code"] = "InternalError"
+        raise ex
+    
+    return groups
+
+def check_group_policies(group_name, admin_accounts, event):
+    # Checks all group policies to ensure that there is not mixture of admin and non-admin roles.
+    
+    # Fetch aws managed and inline group policies
+    managed_policies = fetch_aws_managed_group_policies(group_name)
+    inline_policies = fetch_inline_group_policies(group_name)
+    
+    # Checks for the aws managed policy AdministratorAccess or an inline policy that gives the same access.
+    has_admin_policy = next((p for p in managed_policies if p.get("PolicyName", "") == "AdministratorAccess"), False) or next((p for p in inline_policies if policy_doc_gives_admin_access(p.get("PolicyDocument", "\{\}"))), False)
+    has_non_admin_policy = next((p for p in managed_policies if p.get("PolicyName", "") != "AdministratorAccess"), False) or next((p for p in inline_policies if not policy_doc_gives_admin_access(p.get("PolicyDocument", "\{\}"))), False)
+    
+    # Does the group have admin policies and non admin policies?
+    if has_admin_policy and has_non_admin_policy:
+        # yes, there is a mixture of admin and non-admin roles attached to the group. Return NON_COMPLIANT evaluation for group
+        return build_evaluation(
+            group_name,
+            "NON_COMPLIANT",
+            event,
+            resource_type=GROUP_RESOURCE_TYPE,
+            annotation=f"Group '{group_name}' has attached policies that contain both admin and non-admin roles."
+        )
+    
+    # Is the group an admin group?
+    if has_admin_policy:
+        # yes, fetch group members and check against admin_accounts
+        group_members = fetch_group_members(group_name, event)
+        only_has_admins = next((m for m in group_members if m.get("UserName", "") not in admin_accounts), True)
+        if not only_has_admins:
+            return build_evaluation(
+                group_name,
+                "NON_COMPLIANT",
+                event,
+                resource_type=GROUP_RESOURCE_TYPE,
+                annotation=f"Group '{group_name}' is an admin group that contains non-admin members."
+            )
+            
+    annotation = f"Group '{group_name}' has policies that only provides admin roles and only has admin members." if has_admin_policy else f"Group '{group_name}' has policies that only provides non-admin roles."
+        
+    return build_evaluation(
+        group_name,
+        "COMPLIANT",
+        event,
+        resource_type=GROUP_RESOURCE_TYPE,
+        annotation=annotation
+    )
+    
+def fetch_group_members(group_name, event):
+    members = []
+    marker = None
+    try:
+        while True:
+            response = AWS_IAM_CLIENT.get_group(GroupName=group_name, Marker=marker) if marker else AWS_IAM_CLIENT.get_group(GroupName=group_name)
+            members = members + response.get("Users", [])
+            marker = response.get("Marker")
+            if not marker:
+                break
+    except botocore.exceptions.ClientError as ex:
+        if "NoSuchEntity" in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = f"Unable to fetch information for group '{group_name}'. No such entity found."
+        else:
+            ex.response["Error"]["Message"] = "InternalError"
+            ex.response["Error"]["Code"] = "InternalError"
+        raise ex
+    
+    return members
+        
+def policy_doc_gives_admin_access(policy_doc: str):
+    document_dict = json.loads(policy_doc)
+    statement = document_dict.get("Statement", [])
+    return len(statement) == 1 and statement[0].get("Effect", "") == "Allow" and statement[0].get("Action", "") == "*" and statement[0].get("Resource", "") == "*"
+        
+def fetch_inline_group_policies(group_name):
+    policies = []
+    marker = None
+    try:
+        while True:
+            response = AWS_IAM_CLIENT.list_group_policies(GroupName=group_name, Marker=marker) if marker else AWS_IAM_CLIENT.list_group_policies(GroupName=group_name)      
+            policies = policies + response.get("PolicyNames", [])
+            marker = response.get("Marker")
+            if not marker:
+                break
+    except botocore.exceptions.ClientError as ex:
+        if "NoSuchEntity" in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = f"Unable to fetch policies for group '{group_name}'. No such entity found."
+        elif "InvalidInput" in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = f"Invalid group name '{group_name}' or marker '{marker}' input received."
+        else:
+            ex.response["Error"]["Message"] = "InternalError"
+            ex.response["Error"]["Code"] = "InternalError"
+        raise ex
+    
+    try:
+        for i in range(len(policies)):
+            policies[i] = AWS_IAM_CLIENT.get_group_policy(group_name, policies[i])
+    except botocore.exceptions.ClientError as ex:
+        if "NoSuchEntity" in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = "Unable to fetch inline policy information. No such entity found."
+        elif "InvalidInput" in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = "Failed to fetch inline policy information. Invalid input."
+        else:
+            ex.response["Error"]["Message"] = "InternalError"
+            ex.response["Error"]["Code"] = "InternalError"
+        
+    return policies
+    
+def fetch_aws_managed_group_policies(group_name):
+    policies = []
+    marker = None
+    try:
+        while True:
+            response = AWS_IAM_CLIENT.list_attached_group_policies(GroupName=group_name, Marker=marker) if marker else AWS_IAM_CLIENT.list_attached_group_policies(GroupName=group_name)      
+            policies = policies + response.get("AttachedPolicies", [])
+            marker = response.get("Marker")
+            if not marker:
+                break
+        return policies
+    except botocore.exceptions.ClientError as ex:
+        if "NoSuchEntity" in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = f"Unable to fetch policies for group '{group_name}'. No such entity found."
+        elif "InvalidInput" in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = f"Invalid group name '{group_name}' or marker '{marker}' input received."
+        else:
+            ex.response["Error"]["Message"] = "InternalError"
+            ex.response["Error"]["Code"] = "InternalError"
+        raise ex
 
 def lambda_handler(event, context):
     """This function is the main entry point for Lambda.
@@ -200,6 +319,7 @@ def lambda_handler(event, context):
     global AWS_CONFIG_CLIENT
     global AWS_IAM_CLIENT
     global AWS_ACCOUNT_ID
+    global AWS_S3_CLIENT
     global EXECUTION_ROLE_NAME
     global AUDIT_ACCOUNT_ID
     global PASSWORD_ASSESSMENT_POLICY
@@ -243,21 +363,55 @@ def lambda_handler(event, context):
 
     AWS_CONFIG_CLIENT = get_client("config", event)
     AWS_IAM_CLIENT = get_client("iam", event)
-
-        # is this a scheduled invokation?
+    AWS_S3_CLIENT = boto3.client("s3")
+    
+    # is this a scheduled invokation?
     if is_scheduled_notification(invoking_event["messageType"]):
         # yes, proceed
-        # Update AWS Config with the evaluation result
-        assessment_result = assess_iam_password_policy()
-        evaluations = [
-            build_evaluation(
-                AWS_ACCOUNT_ID,
-                assessment_result["status"],
-                event,
-                resource_type="AWS::::Account",
-                annotation=assessment_result["annotation"],
+        
+        admin_accounts_file_path = valid_rule_parameters.get("AdminAccountsFilePath", "")
+        if check_s3_object_exists(admin_accounts_file_path) == False:
+            evaluations.append(
+                build_evaluation(
+                    event["accountId"],
+                    "NON_COMPLIANT",
+                    event,
+                    resource_type=DEFAULT_RESOURCE_TYPE,
+                    annotation="No AdminAccountsFilePath input provided.",
+                )
             )
-        ]
+        else:     
+            is_compliant = True
+            admin_accounts_json_list = read_s3_object(admin_accounts_file_path)
+            admin_accounts = json.loads(admin_accounts_json_list)
+            
+            groups = fetch_groups()
+            for g in groups:
+                eval = check_group_policies(g.get("GroupName", ""), admin_accounts, event)
+                if eval.get("ComplianceType", "NON_COMPLIANT") == "NON_COMPLIANT":
+                    is_compliant = False
+            
+            if is_compliant:
+                evaluations.append(
+                    build_evaluation(
+                        AWS_ACCOUNT_ID,
+                        "COMPLIANT",
+                        event,
+                        resource_type=DEFAULT_RESOURCE_TYPE,
+                        annotation="Account groups only have admin or only have non-admin roles, and admin groups only have admin members.",
+                    )
+                )
+            else:
+                evaluations.append(
+                    build_evaluation(
+                        AWS_ACCOUNT_ID,
+                        "NON_COMPLIANT",
+                        event,
+                        resource_type=DEFAULT_RESOURCE_TYPE,
+                        annotation="Account groups do not only have admin or only have non-admin roles, and admin groups only have admin members.",
+                    )
+                )
+                
         AWS_CONFIG_CLIENT.put_evaluations(
             Evaluations=evaluations,
             ResultToken=event["resultToken"]
