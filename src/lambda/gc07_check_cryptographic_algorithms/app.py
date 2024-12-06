9e8155c51ac17c4afce6165921949d80779bb69e
David Dearden <David.Dearden@ssc-spc.gc.ca>
2024-12-06 09:54:49 -0800

    feat(gr07.2): Update for changes in specification

--- src/lambda/gc07_check_cryptographic_algorithms/app.py
+++ src/lambda/gc07_check_cryptographic_algorithms/app.py
@@ -51,12 +51,6 @@ def evaluate_parameters(rule_parameters):
     Keyword arguments:
     rule_parameters -- the Key/Value dictionary of the Config Rule parameters
     """
-    if "s3ObjectPath" not in rule_parameters:
-        logger.error('The parameter with "s3ObjectPath" as key must be defined.')
-        raise ValueError('The parameter with "s3ObjectPath" as key must be defined.')
-    if not rule_parameters["s3ObjectPath"]:
-        logger.error('The parameter "s3ObjectPath" must have a defined value.')
-        raise ValueError('The parameter "s3ObjectPath" must have a defined value.')
     return rule_parameters
 
 
@@ -125,39 +119,6 @@ def is_scheduled_notification(message_type):
     return message_type == "ScheduledNotification"
 
 
-def check_s3_object_exists(aws_s3_client, object_path: str) -> bool:
-    """Check if the S3 object exists
-    Keyword arguments:
-    object_path -- the S3 object path
-    """
-    # parse the S3 path
-    match = re.match(r"s3:\/\/([^/]+)\/((?:[^/]*/)*.*)", object_path)
-    if match:
-        bucket_name = match.group(1)
-        key_name = match.group(2)
-    else:
-        logger.error("Unable to parse S3 object path %s", object_path)
-        raise ValueError(f"Unable to parse S3 object path {object_path}")
-    try:
-        aws_s3_client.head_object(Bucket=bucket_name, Key=key_name)
-    except botocore.exceptions.ClientError as err:
-        if err.response["Error"]["Code"] == "404":
-            # The object does not exist.
-            logger.info("Object %s not found in bucket %s", key_name, bucket_name)
-            return False
-        elif err.response["Error"]["Code"] == "403":
-            # AccessDenied
-            logger.info("Access denied to bucket %s", bucket_name)
-            return False
-        else:
-            # Something else has gone wrong.
-            logger.error("Error trying to find object %s in bucket %s", key_name, bucket_name)
-            raise ValueError(f"Error trying to find object {key_name} in bucket {bucket_name}") from err
-    else:
-        # The object does exist.
-        return True
-
-
 def elb_describe_load_balancer_policies(elb_client, load_balancer_name: str, policy_names: list[str]) -> list[dict]:
     try:
         response = elb_client.describe_load_balancer_policies(
@@ -179,7 +140,7 @@ def elb_describe_load_balancer_policies(elb_client, load_balancer_name: str, pol
 
 
 def elb_describe_all_load_balancers(
-    elb_client, page_size: int = 400, interval_between_calls: float = 0.25
+    elb_client, page_size: int = 400, interval_between_calls: float = 0.1
 ) -> list[dict]:
     """
     Get a list of all the Classic Load Balancers
@@ -253,9 +214,7 @@ def policy_protocols_and_cipher_suites_meet_recommendations(lb_name: str, polici
     return all_meet_recommendations
 
 
-def assess_load_balancer_enforcement(
-    load_balancers_details: list[dict], attestation_file_exists: bool, event
-) -> list[dict]:
+def assess_load_balancer_enforcement(load_balancers_details: list[dict], event) -> list[dict]:
     resource_type = "AWS::ElasticLoadBalancing::LoadBalancer"
     evaluations = []
     all_resources_are_compliant = True
@@ -266,26 +225,19 @@ def assess_load_balancer_enforcement(
         custom_policies = extract_custom_policies(load_balancer_details.get("Policies", []))
         logger.info("Custom Policies found for Load Balancer '%s': %s", lb_name, custom_policies)
 
-        if custom_policies:
-            if policy_protocols_and_cipher_suites_meet_recommendations(lb_name, custom_policies):
-                if attestation_file_exists:
-                    annotation = "The load balancer is using a custom policy and the protocol and cipher suite meet recommendations, but the attestation file exists."
-                    compliance_type = "COMPLIANT"
-                else:
-                    annotation = "The load balancer is using a custom policy and the protocol and cipher suite meet recommendations, but the attestation file does NOT exist."
-                    compliance_type = "NON_COMPLIANT"
-            else:
-                annotation = "The load balancer is using a custom policy, but the protocol and/or cipher suite do NOT meet recommendations."
-                compliance_type = "NON_COMPLIANT"
+        if not custom_policies:
+            compliance_type = "COMPLIANT"
+            annotation = "The load balancer is NOT using a custom policy."
+        elif policy_protocols_and_cipher_suites_meet_recommendations(lb_name, custom_policies):
+            compliance_type = "COMPLIANT"
+            annotation = (
+                "The load balancer is using a custom policy and the protocol and cipher suite meet recommendations."
+            )
         else:
-            if attestation_file_exists:
-                annotation = "The load balancer is NOT using a custom policy, but the attestation file exists."
-                compliance_type = "COMPLIANT"
-            else:
-                annotation = "The load balancer is NOT using a custom policy, but the attestation file does NOT exist."
-                compliance_type = "NON_COMPLIANT"
+            compliance_type = "NON_COMPLIANT"
+            annotation = "The load balancer is using a custom policy, but the protocol and/or cipher suite do NOT meet recommendations."
 
-        logger.info(f"{annotation} LoadBalancerName: '{resource_id}'")
+        logger.info(f"{annotation} LoadBalancerName: '{lb_name}'")
         evaluations.append(build_evaluation(resource_id, compliance_type, event, resource_type, annotation))
         if compliance_type == "NON_COMPLIANT":
             all_resources_are_compliant = False
@@ -294,7 +246,7 @@ def assess_load_balancer_enforcement(
 
 
 def submit_evaluations(
-    aws_config_client, result_token: str, evaluations: list[dict], interval_between_calls: float = 0.25
+    aws_config_client, result_token: str, evaluations: list[dict], interval_between_calls: float = 0.1
 ):
     max_evaluations_per_call = 100
     while evaluations:
@@ -322,7 +274,7 @@ def lambda_handler(event, context):
     global EXECUTION_ROLE_NAME
 
     page_size = 100
-    interval_between_api_calls = 0.25
+    interval_between_api_calls = 0.1
 
     rule_parameters = json.loads(event.get("ruleParameters", "{}"))
     invoking_event = json.loads(event.get("invokingEvent", "{}"))
@@ -340,8 +292,6 @@ def lambda_handler(event, context):
         return
 
     aws_config_client = get_client("config", event)
-    # Not using get_client to get S3 client for the Audit account
-    aws_s3_client = boto3.client("s3")
     # Using older 'elb' api instead of 'elbv2' so that we get the 'Classic' load balancers
     aws_elb_client = get_client("elb", event)
 
@@ -355,25 +305,15 @@ def lambda_handler(event, context):
         for x in elb_describe_all_load_balancers(aws_elb_client, page_size, interval_between_api_calls)
     ]
 
-    attestation_file_exists = check_s3_object_exists(aws_s3_client, valid_rule_parameters["s3ObjectPath"])
-    evaluations, all_resources_are_compliant = assess_load_balancer_enforcement(
-        load_balancers_details, attestation_file_exists, event
-    )
+    evaluations, all_resources_are_compliant = assess_load_balancer_enforcement(load_balancers_details, event)
 
-    status = "COMPLIANT" if all_resources_are_compliant and attestation_file_exists else "NON_COMPLIANT"
-    resources_annotation = (
-        "All resources found are compliant."
-        if all_resources_are_compliant
-        else "Non-compliant resources found in scope."
-    )
-    file_annotation = (
-        "Cryptographic Algorithms Attestation file exists."
-        if attestation_file_exists
-        else "Cryptographic Algorithms Attestation file DOES NOT exist."
-    )
-    annotation = f"{resources_annotation} {file_annotation}"
+    if all_resources_are_compliant:
+        compliance_type = "COMPLIANT"
+        annotation = "All resources found are compliant."
+    else:
+        compliance_type = "NON_COMPLIANT"
+        annotation = "Non-compliant resources found in scope."
 
-    logger.info(f"{status}: {annotation}")
-    evaluations.append(build_evaluation(AWS_ACCOUNT_ID, status, event, ACCOUNT_RESOURCE_TYPE, annotation))
-
+    logger.info(f"{compliance_type}: {annotation}")
+    evaluations.append(build_evaluation(AWS_ACCOUNT_ID, compliance_type, event, ACCOUNT_RESOURCE_TYPE, annotation))
     submit_evaluations(aws_config_client, event["resultToken"], evaluations, interval_between_api_calls)
