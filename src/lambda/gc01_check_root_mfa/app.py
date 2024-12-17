e90b8763e8a237c768965652c89925a6bf231370
David Dearden <David.Dearden@ssc-spc.gc.ca>
2024-12-17 12:42:27 -0800

    Make ExecutionRoleName a required parameter

--- src/lambda/gc01_check_root_mfa/app.py
+++ src/lambda/gc01_check_root_mfa/app.py
@@ -130,8 +130,8 @@ def lambda_handler(event, context):
         return
 
     rule_parameters = json.loads(event.get("ruleParameters", "{}"))
-    valid_rule_parameters = check_required_parameters(rule_parameters, [])
-    execution_role_name = valid_rule_parameters.get("ExecutionRoleName", "AWSA-GCLambdaExecutionRole")
+    valid_rule_parameters = check_required_parameters(rule_parameters, ["ExecutionRoleName"])
+    execution_role_name = valid_rule_parameters.get("ExecutionRoleName")
     audit_account_id = valid_rule_parameters.get("AuditAccountID", "")
     aws_account_id = event["accountId"]
     is_not_audit_account = aws_account_id != audit_account_id
