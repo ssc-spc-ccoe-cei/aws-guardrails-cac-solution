29bbb7bc6f656d484cc68b438c7de6ec8aa73c8a
TylerTMizuyabu <tyler.mizuyabu@ssc-spc.gc.ca>
2024-12-18 16:54:23 -0500

    added use of functions to check alerts flag lambda

--- src/lambda/gc01_check_alerts_flag_misuse/app.py
+++ src/lambda/gc01_check_alerts_flag_misuse/app.py
@@ -8,8 +8,9 @@ import re
 
 import botocore.exceptions
 
-from utils import is_scheduled_notification, check_required_parameters
+from utils import is_scheduled_notification, check_required_parameters, check_guardrail_rquirement_by_cloud_usage_profile, get_cloud_profile_from_tags, GuardrailType, GuardrailRequirementType
 from boto_util.client import get_client
+from boto_util.organizations import get_account_tags
 from boto_util.config import build_evaluation, submit_evaluations
 from boto_util.s3 import check_s3_object_exists, read_s3_object
 from boto_util.guard_duty import guard_duty_is_enabled
@@ -214,6 +215,8 @@ def lambda_handler(event, context):
                         annotation="GuardDuty is not enabled and there are no EventBridge rules setup to notify authorized personnel of misuse or suspicious activity.",
                     )
                 )
+                
+    return evaluations
 
     logger.info("AWS Config updating evaluations: %s", evaluations)
     submit_evaluations(aws_config_client, event["resultToken"], evaluations)
