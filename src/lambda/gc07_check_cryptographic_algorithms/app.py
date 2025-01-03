c2b39bd0cf2dedac897503e7beb71cf3f4999f0b
David Dearden <David.Dearden@ssc-spc.gc.ca>
2025-01-03 15:36:58 -0800

    Refactor all gc07

--- src/lambda/gc07_check_cryptographic_algorithms/app.py
+++ src/lambda/gc07_check_cryptographic_algorithms/app.py
@@ -3,14 +3,16 @@
 
 import json
 import logging
-import time
-import re
 
-import boto3
-import botocore
+from utils import is_scheduled_notification, check_required_parameters, flat
+from boto_util.client import get_client
+from boto_util.config import build_evaluation, submit_evaluations
+from boto_util.elb import describe_elb_load_balancer_policies, describe_all_elb_load_balancers
 
-ASSUME_ROLE_MODE = True
-ACCOUNT_RESOURCE_TYPE = "AWS::::Account"
+
+# Logging setup
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # See https://www.cyber.gc.ca/en/guidance/guidance-securely-configuring-network-protocols-itsp40062
 #   section 3.1 TLS Cipher suites (Table 2 and Table 3)
@@ -46,115 +48,6 @@ ITSP_40_062_ALLOWED_SUITES = {
 }
 
 
-def evaluate_parameters(rule_parameters):
-    """Evaluate the rule parameters dictionary validity. Raise a Exception for invalid parameters.
-    Keyword arguments:
-    rule_parameters -- the Key/Value dictionary of the Config Rule parameters
-    """
-    return rule_parameters
-
-
-def build_evaluation(resource_id, compliance_type, event, resource_type, annotation=None):
-    """Form an evaluation as a dictionary. Usually suited to report on scheduled rules.
-    Keyword arguments:
-    resource_id -- the unique id of the resource to report
-    compliance_type -- either COMPLIANT, NON_COMPLIANT or NOT_APPLICABLE
-    event -- the event variable given in the lambda handler
-    resource_type -- the CloudFormation resource type (or AWS::::Account) to report on the rule
-    annotation -- an annotation to be added to the evaluation (default None)
-    """
-    eval_cc = {}
-    if annotation:
-        eval_cc["Annotation"] = annotation
-    eval_cc["ComplianceResourceType"] = resource_type
-    eval_cc["ComplianceResourceId"] = resource_id
-    eval_cc["ComplianceType"] = compliance_type
-    eval_cc["OrderingTimestamp"] = str(json.loads(event["invokingEvent"])["notificationCreationTime"])
-    return eval_cc
-
-
-def get_client(service, event, region="ca-central-1"):
-    """Return the service boto client. It should be used instead of directly calling the client.
-    Keyword arguments:
-    service -- the service name used for calling the boto.client()
-    event -- the event variable given in the lambda handler
-    """
-    if not ASSUME_ROLE_MODE or (AWS_ACCOUNT_ID == AUDIT_ACCOUNT_ID):
-        return boto3.client(service, region_name=region)
-    execution_role_arn = f"arn:aws:iam::{AWS_ACCOUNT_ID}:role/{EXECUTION_ROLE_NAME}"
-    credentials = get_assume_role_credentials(execution_role_arn, region)
-    return boto3.client(
-        service,
-        region_name=region,
-        aws_access_key_id=credentials["AccessKeyId"],
-        aws_secret_access_key=credentials["SecretAccessKey"],
-        aws_session_token=credentials["SessionToken"],
-    )
-
-
-def get_assume_role_credentials(role_arn, region="ca-central-1"):
-    """Return the service boto client. It should be used instead of directly calling the client.
-    Keyword arguments:
-    service -- the service name used for calling the boto.client()
-    event -- the event variable given in the lambda handler
-    """
-    sts_client = boto3.client("sts", region_name=region)
-    try:
-        assume_role_response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName="configLambdaExecution")
-        return assume_role_response["Credentials"]
-    except botocore.exceptions.ClientError as ex:
-        if "AccessDenied" in ex.response["Error"]["Code"]:
-            ex.response["Error"]["Message"] = "AWS Config does not have permission to assume the IAM role."
-        else:
-            ex.response["Error"]["Message"] = "InternalError"
-            ex.response["Error"]["Code"] = "InternalError"
-        raise ex
-
-
-def is_scheduled_notification(message_type):
-    """Check whether the message is a ScheduledNotification or not.
-    Keyword arguments:
-    message_type -- the message type
-    """
-    return message_type == "ScheduledNotification"
-
-
-def elb_describe_load_balancer_policies(elb_client, load_balancer_name: str, policy_names: list[str]) -> list[dict]:
-    try:
-        response = elb_client.describe_load_balancer_policies(
-            LoadBalancerName=load_balancer_name, PolicyNames=policy_names
-        )
-        return [] if not response else response.get("PolicyDescriptions", [])
-    except botocore.exceptions.ClientError as ex:
-        # Scrub error message for any internal account info leaks
-        if "AccessDenied" in ex.response["Error"]["Code"]:
-            ex.response["Error"]["Message"] = "AWS Config does not have permission to assume the IAM role."
-        elif "AccessPointNotFound" in ex.response["Error"]["Code"]:
-            ex.response["Error"]["Message"] = "AccessPointNotFound Error calling 'describe_load_balancer_policies'"
-        elif "PolicyNotFound" in ex.response["Error"]["Code"]:
-            ex.response["Error"]["Message"] = "PolicyNotFound Error calling 'describe_load_balancer_policies'"
-        else:
-            ex.response["Error"]["Message"] = "InternalError"
-            ex.response["Error"]["Code"] = "InternalError"
-        raise ex
-
-
-def elb_describe_all_load_balancers(
-    elb_client, page_size: int = 400, interval_between_calls: float = 0.1
-) -> list[dict]:
-    """
-    Get a list of all the Classic Load Balancers
-    https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elb/paginator/DescribeLoadBalancers.html
-    """
-    resources: list[dict] = []
-    paginator = elb_client.get_paginator("describe_load_balancers")
-    page_iterator = paginator.paginate(PaginationConfig={"PageSize": page_size})
-    for page in page_iterator:
-        resources.extend(page.get("LoadBalancerDescriptions", []))
-        time.sleep(interval_between_calls)
-    return resources
-
-
 def extract_custom_policies(policies: list[dict]) -> list[dict]:
     return [
         policy
@@ -162,10 +55,6 @@ def extract_custom_policies(policies: list[dict]) -> list[dict]:
         if not policy.get("PolicyName", "").startswith("ELBSecurityPolicy")
         and not policy.get("PolicyName", "").startswith("ELBSample")
     ]
-
-
-def flat(list):
-    return [x for sub_list in list for x in sub_list]
 
 
 def policy_protocols_and_cipher_suites_meet_recommendations(lb_name: str, policies: list[dict]) -> bool:
@@ -245,64 +134,44 @@ def assess_load_balancer_enforcement(load_balancers_details: list[dict], event) 
     return evaluations, all_resources_are_compliant
 
 
-def submit_evaluations(
-    aws_config_client, result_token: str, evaluations: list[dict], interval_between_calls: float = 0.1
-):
-    max_evaluations_per_call = 100
-    while evaluations:
-        batch_of_evaluations, evaluations = (
-            evaluations[:max_evaluations_per_call],
-            evaluations[max_evaluations_per_call:],
-        )
-        aws_config_client.put_evaluations(Evaluations=batch_of_evaluations, ResultToken=result_token)
-        if evaluations:
-            time.sleep(interval_between_calls)
-
-
 def lambda_handler(event, context):
-    """Lambda handler to check CloudTrail trails are logging.
+    """
+    This function is the main entry point for Lambda.
+
     Keyword arguments:
+
     event -- the event variable given in the lambda handler
+
     context -- the context variable given in the lambda handler
     """
-    global logger
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    logger.info("Received Event: %s", json.dumps(event, indent=2))
 
-    global AWS_ACCOUNT_ID
-    global AUDIT_ACCOUNT_ID
-    global EXECUTION_ROLE_NAME
-
-    page_size = 100
-    interval_between_api_calls = 0.1
-
-    rule_parameters = json.loads(event.get("ruleParameters", "{}"))
-    invoking_event = json.loads(event.get("invokingEvent", "{}"))
-    logger.info("Received event: %s", json.dumps(event, indent=2))
-
-    AWS_ACCOUNT_ID = event["accountId"]
-    logger.info("Assessing account %s", AWS_ACCOUNT_ID)
-
-    valid_rule_parameters = evaluate_parameters(rule_parameters)
-    EXECUTION_ROLE_NAME = valid_rule_parameters.get("ExecutionRoleName", "AWSA-GCLambdaExecutionRole")
-    AUDIT_ACCOUNT_ID = valid_rule_parameters.get("AuditAccountID", "")
-
+    invoking_event = json.loads(event["invokingEvent"])
     if not is_scheduled_notification(invoking_event["messageType"]):
         logger.error("Skipping assessments as this is not a scheduled invocation")
         return
 
-    aws_config_client = get_client("config", event)
+    rule_parameters = check_required_parameters(json.loads(event.get("ruleParameters", "{}")), ["ExecutionRoleName"])
+    execution_role_name = rule_parameters.get("ExecutionRoleName")
+    audit_account_id = rule_parameters.get("AuditAccountID", "")
+    aws_account_id = event["accountId"]
+    is_not_audit_account = aws_account_id != audit_account_id
+
+    page_size = 100
+    interval_between_api_calls = 0.1
+
+    aws_config_client = get_client("config", aws_account_id, execution_role_name, is_not_audit_account)
     # Using older 'elb' api instead of 'elbv2' so that we get the 'Classic' load balancers
-    aws_elb_client = get_client("elb", event)
+    aws_elb_client = get_client("elb", aws_account_id, execution_role_name, is_not_audit_account)
 
     load_balancers_details = [
         {
             "Description": x,
-            "Policies": elb_describe_load_balancer_policies(
+            "Policies": describe_elb_load_balancer_policies(
                 aws_elb_client, x.get("LoadBalancerName"), x.get("Policies", {}).get("OtherPolicies", [])
             ),
         }
-        for x in elb_describe_all_load_balancers(aws_elb_client, page_size, interval_between_api_calls)
+        for x in describe_all_elb_load_balancers(aws_elb_client, page_size, interval_between_api_calls)
     ]
 
     evaluations, all_resources_are_compliant = assess_load_balancer_enforcement(load_balancers_details, event)
@@ -315,5 +184,5 @@ def lambda_handler(event, context):
         annotation = "Non-compliant resources found in scope."
 
     logger.info(f"{compliance_type}: {annotation}")
-    evaluations.append(build_evaluation(AWS_ACCOUNT_ID, compliance_type, event, ACCOUNT_RESOURCE_TYPE, annotation))
+    evaluations.append(build_evaluation(aws_account_id, compliance_type, event, annotation=annotation))
     submit_evaluations(aws_config_client, event["resultToken"], evaluations, interval_between_api_calls)
