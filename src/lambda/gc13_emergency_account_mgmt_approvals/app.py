69bea40c408d594a515314f3eafdf8fc6a23b4c2
David Dearden <David.Dearden@ssc-spc.gc.ca>
2025-01-07 10:17:52 -0800

    Refactor all gc13

--- src/lambda/gc13_emergency_account_mgmt_approvals/app.py
+++ src/lambda/gc13_emergency_account_mgmt_approvals/app.py
@@ -1,212 +1,65 @@
 """ GC13 - Check Emergency Account Management Procedure Approvals Lambda Function
     Providing documents as evidence to support the Guardrails
 """
+
 import json
 import logging
-import re
 
-import boto3
-import botocore
+from utils import is_scheduled_notification, check_required_parameters
+from boto_util.client import get_client
+from boto_util.config import build_evaluation, submit_evaluations
+from boto_util.s3 import check_s3_object_exists
 
 # Logging setup
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Set to True to get the lambda to assume the Role attached on the Config Service
-ASSUME_ROLE_MODE = True
-DEFAULT_RESOURCE_TYPE = "AWS::::Account"
-
-
-# This gets the client after assuming the Config service role
-# either in the same AWS account or cross-account.
-def get_client(service, event):
-    """Return the service boto client. It should be used instead of directly calling the client.
-    Keyword arguments:
-    service -- the service name used for calling the boto.client()
-    event -- the event variable given in the lambda handler
-    """
-    if not ASSUME_ROLE_MODE or (AWS_ACCOUNT_ID == AUDIT_ACCOUNT_ID):
-        return boto3.client(service)
-    execution_role_arn = f"arn:aws:iam::{AWS_ACCOUNT_ID}:role/{EXECUTION_ROLE_NAME}"
-    credentials = get_assume_role_credentials(execution_role_arn)
-    return boto3.client(
-        service,
-        aws_access_key_id=credentials["AccessKeyId"],
-        aws_secret_access_key=credentials["SecretAccessKey"],
-        aws_session_token=credentials["SessionToken"],
-    )
-
-
-def get_assume_role_credentials(role_arn):
-    """Returns the credentials required to assume the passed role
-    Keyword arguments:
-    role_arn -- the arn of the role to assume"""
-    sts_client = boto3.client("sts")
-    try:
-        assume_role_response = sts_client.assume_role(
-            RoleArn=role_arn,
-            RoleSessionName="configLambdaExecution"
-        )
-        return assume_role_response["Credentials"]
-    except botocore.exceptions.ClientError as ex:
-        # Scrub error message for any internal account info leaks
-        if "AccessDenied" in ex.response["Error"]["Code"]:
-            ex.response["Error"]["Message"] = "AWS Config does not have permission to assume the IAM role."
-        else:
-            ex.response["Error"]["Message"] = "InternalError"
-            ex.response["Error"]["Code"] = "InternalError"
-        logger.error("ERROR assuming role. %s", ex.response["Error"])
-        raise ex
-
-
-# Check whether the message is a ScheduledNotification or not.
-def is_scheduled_notification(message_type):
-    """Check whether the message is a ScheduledNotification or not.
-    Keyword arguments:
-    message_type -- the message type
-    """
-    return message_type == "ScheduledNotification"
-
-
-def evaluate_parameters(rule_parameters):
-    """Evaluate the rule parameters dictionary validity. Raise a Exception for invalid parameters.
-    Keyword arguments:
-    rule_parameters -- the Key/Value dictionary of the Config Rule parameters
-    """
-    if "s3ObjectPath" not in rule_parameters:
-        logger.error('The parameter with "s3ObjectPath" as key must be defined.')
-        raise ValueError('The parameter with "s3ObjectPath" as key must be defined.')
-    if not rule_parameters["s3ObjectPath"]:
-        logger.error('The parameter "s3ObjectPath" must have a defined value.')
-        raise ValueError('The parameter "s3ObjectPath" must have a defined value.')
-    return rule_parameters
-
-
-def check_s3_object_exists(object_path):
-    """Check if the S3 object exists
-    Keyword arguments:
-    object_path -- the S3 object path
-    """
-    # parse the S3 path
-    match = re.match(r"s3:\/\/([^/]+)\/((?:[^/]*/)*.*)", object_path)
-    if match:
-        bucket_name = match.group(1)
-        key_name = match.group(2)
-    else:
-        logger.error("Unable to parse S3 object path %s", object_path)
-        raise ValueError(f"Unable to parse S3 object path {object_path}")
-    try:
-        AWS_S3_CLIENT.head_object(Bucket=bucket_name, Key=key_name)
-    except botocore.exceptions.ClientError as err:
-        if err.response["Error"]["Code"] == "404":
-            # The object does not exist.
-            logger.info("Object %s not found in bucket %s", key_name, bucket_name)
-            return False
-        elif err.response["Error"]["Code"] == "403":
-            # AccessDenied
-            logger.info("Access denied to bucket %s", bucket_name)
-            return False
-        else:
-            # Something else has gone wrong.
-            logger.error("Error trying to find object %s in bucket %s", key_name, bucket_name)
-            raise ValueError(f"Error trying to find object {key_name} in bucket {bucket_name}") from err
-    else:
-        # The object does exist.
-        return True
-
-
-# This generate an evaluation for config
-def build_evaluation(resource_id, compliance_type, event, resource_type=DEFAULT_RESOURCE_TYPE, annotation=None):
-    """Form an evaluation as a dictionary. Usually suited to report on scheduled rules.
-    Keyword arguments:
-    resource_id -- the unique id of the resource to report
-    compliance_type -- either COMPLIANT, NON_COMPLIANT or NOT_APPLICABLE
-    event -- the event variable given in the lambda handler
-    resource_type -- the CloudFormation resource type (or AWS::::Account)
-    to report on the rule (default DEFAULT_RESOURCE_TYPE)
-    annotation -- an annotation to be added to the evaluation (default None)
-    """
-    eval_cc = {}
-    if annotation:
-        eval_cc["Annotation"] = annotation
-    eval_cc["ComplianceResourceType"] = resource_type
-    eval_cc["ComplianceResourceId"] = resource_id
-    eval_cc["ComplianceType"] = compliance_type
-    eval_cc["OrderingTimestamp"] = str(json.loads(event["invokingEvent"])["notificationCreationTime"])
-    return eval_cc
-
 
 def lambda_handler(event, context):
     """
-    This function is the main handler for the Lambda function.
-    It will call the appropriate functions based on the event type. Keyword arguments:
+    This function is the main entry point for Lambda.
+
+    Keyword arguments:
+
     event -- the event variable given in the lambda handler
+
     context -- the context variable given in the lambda handler
     """
-
-    global AWS_CONFIG_CLIENT
-    global AWS_S3_CLIENT
-    global AWS_ACCOUNT_ID
-    global EXECUTION_ROLE_NAME
-    global AUDIT_ACCOUNT_ID
-
-    evaluations = []
-    rule_parameters = {}
-    invoking_event = json.loads(event["invokingEvent"])
     logger.info("Received Event: %s", json.dumps(event, indent=2))
 
-    # parse parameters
-    AWS_ACCOUNT_ID = event["accountId"]
-    if "ruleParameters" in event:
-        rule_parameters = json.loads(event["ruleParameters"])
+    invoking_event = json.loads(event["invokingEvent"])
+    if not is_scheduled_notification(invoking_event["messageType"]):
+        logger.error("Skipping assessments as this is not a scheduled invocation")
+        return
 
-    valid_rule_parameters = evaluate_parameters(rule_parameters)
+    rule_parameters = check_required_parameters(
+        json.loads(event.get("ruleParameters", "{}")), ["ExecutionRoleName", "s3ObjectPath"]
+    )
+    execution_role_name = rule_parameters.get("ExecutionRoleName")
+    audit_account_id = rule_parameters.get("AuditAccountID", "")
+    aws_account_id = event["accountId"]
+    is_not_audit_account = aws_account_id != audit_account_id
 
-    if "ExecutionRoleName" in valid_rule_parameters:
-        EXECUTION_ROLE_NAME = valid_rule_parameters["ExecutionRoleName"]
+    evaluations = []
+
+    # This check only applies to the audit account
+    if is_not_audit_account:
+        logger.info(
+            "Emergency Account Management Procedure Approval not checked in account %s - not the Audit account",
+            aws_account_id,
+        )
+        return
+
+    aws_config_client = get_client("config")
+    aws_s3_client = get_client("s3")
+
+    if check_s3_object_exists(aws_s3_client, rule_parameters["s3ObjectPath"]):
+        compliance_type = "COMPLIANT"
+        annotation = "Emergency Account Management Procedure Approval found"
     else:
-        EXECUTION_ROLE_NAME = "AWSA-GCLambdaExecutionRole"
+        compliance_type = "NON_COMPLIANT"
+        annotation = "Emergency Account Management Procedure Approval NOT found"
 
-    if "AuditAccountID" in valid_rule_parameters:
-        AUDIT_ACCOUNT_ID = valid_rule_parameters["AuditAccountID"]
-    else:
-        AUDIT_ACCOUNT_ID = ""
-
-    compliance_value = "NOT_APPLICABLE"
-    custom_annotation = "Guardrail only applicable in the Audit Account"
-
-    # is this a scheduled invocation?
-    if is_scheduled_notification(invoking_event["messageType"]):
-        # yes, proceed
-        # is this being executed against the Audit Account -
-        # (this check only applies to the audit account)
-        if AWS_ACCOUNT_ID == AUDIT_ACCOUNT_ID:
-            # yes, we're in the Audit account
-            AWS_CONFIG_CLIENT = get_client("config", event)
-            AWS_S3_CLIENT = get_client("s3", event)
-            # check if object exists in S3
-            if check_s3_object_exists(valid_rule_parameters["s3ObjectPath"]):
-                compliance_value = "COMPLIANT"
-                custom_annotation = "Emergency Account Management Procedure Approval found"
-                logger.info('COMPLIANT - Emergency Account Management Procedure Approval found in S3 at "%s"', valid_rule_parameters["s3ObjectPath"])
-            else:
-                compliance_value = "NON_COMPLIANT"
-                custom_annotation = "Emergency Account Management Procedure Approval NOT found"
-                logger.info("NON_COMPLIANT - Emergency Account Management Procedure Approval not found in S3")
-            # Update AWS Config with the evaluation result
-            evaluations.append(
-                build_evaluation(
-                    event["accountId"],
-                    compliance_value,
-                    event,
-                    resource_type=DEFAULT_RESOURCE_TYPE,
-                    annotation=custom_annotation,
-                )
-            )
-            AWS_CONFIG_CLIENT.put_evaluations(
-                Evaluations=evaluations,
-                ResultToken=event["resultToken"]
-            )
-        else:
-            logger.info("Emergency Account Management Procedure Approval not checked in account %s - not the Audit account", AWS_ACCOUNT_ID)
-
+    logger.info(f"{compliance_type}: {annotation}")
+    evaluations.append(build_evaluation(aws_account_id, compliance_type, event, annotation=annotation))
+    submit_evaluations(aws_config_client, event["resultToken"], evaluations)
